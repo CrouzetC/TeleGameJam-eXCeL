f48112d1471806fcf525fc0438f31b816b7737b3
Louise Bulone <loubu@live.fr>
2020-05-22 20:21:26 +0200

    Ajout d'action

---
+++ src/games/test/week/actions/Study.java
@@ -0,0 +1,45 @@
+package games.test.week.actions;
+
+import java.awt.Font;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Study implements ActionEvent {
+
+    Font awtFont;
+    TrueTypeFont font;
+    boolean isOver;
+
+    public Study() {
+        awtFont = new Font("vt323", java.awt.Font.BOLD, 12);
+        font = new TrueTypeFont(awtFont, true);
+        isOver = false;
+    }
+
+    public void update(GameContainer container, StateBasedGame game, int delta) {
+
+    }
+
+    public void render(GameContainer container, StateBasedGame game, Graphics context) {
+
+        font.drawString(20, 20, "You are studying", Color.red);
+
+    }
+
+    public void keyPressed(int key, char c) {
+        if (c=='a')
+            isOver = true;
+    }
+
+    public void mousePressed(int button, int x, int y) {
+
+    }
+
+    public boolean isOver() {
+        return isOver;
+    }
+
+}
