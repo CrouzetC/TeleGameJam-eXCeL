d1eb6cd0c88e6dd9dae90b1ae5700bbe03c402de
Louise Bulone <loubu@live.fr>
2020-05-22 23:33:40 +0200

    Ajout d'image background menu

--- src/games/test/week/ActionMenu.java
+++ src/games/test/week/ActionMenu.java
@@ -1,27 +1,51 @@
 package games.test.week;
 
+
 import games.test.data.GameData;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Image;
+
+import app.AppLoader;
 
 public class ActionMenu {
 
     private GameData data;
+    private Week week;
+
+// Rendering :
+    private Image background;
+    private Image clubs;
+    private Image sleep;
+    private Image socialize;
+    private Image study;
+    private Image dateTime;
+
 
     public ActionMenu(GameData data) {
 
     }
 
-    public void init() {
+    public void setWeek(Week week) {
+        this.week = week;
+    }
 
+    public void init() {
+        try {
+            this.background =  AppLoader.loadPicture ("/images/home.png");
+        }catch(Exception e){
+            System.out.printf("PROBLEM !");
+        }
     }
 
     public void update(GameContainer container, StateBasedGame game, int delta) {
-
     }
 
     public void render(GameContainer container, StateBasedGame game, Graphics context) {
+        if(this.background != null){
+            this.background.draw();
+        }
 
     }
 
