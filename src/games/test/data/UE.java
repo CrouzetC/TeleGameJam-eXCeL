cbf59c7246c55da9e8e59a3431df9b5b8cbac8ad
xavier.indice@telecomnancy.eu <xavier.indice@telecomnancy.eu>
2020-05-23 03:00:44 +0200

    Classes UE, Player, Project modifiées

--- src/games/test/data/UE.java
+++ src/games/test/data/UE.java
@@ -3,11 +3,37 @@ package games.test.data;
 public class UE {
 
     private int nbPoints;
+    private int threshold;
     private String name;
 
-    public UE (String name) {
+    public UE (String name, int t) {
         this.name = name;
         this.nbPoints = 0;
+        this.threshold = t;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNbPoints() {
+        return nbPoints;
+    }
+
+    public void setNbPoints(int nbPoints) {
+        this.nbPoints = nbPoints;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
     }
 
 }
